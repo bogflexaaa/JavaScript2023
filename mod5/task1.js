678c512b3d06f17f834ea177265ff3d0e4faf34b
bogflexaaa <127213832+bogflexaaa@users.noreply.github.com>
2023-12-11 20:17:38 +0500

    Add files via upload

---
+++ mod5/task1.js
@@ -0,0 +1,10 @@
+function getAge(birthYear){   
+  
+    return currentYear - birthYear; 
+} 
+ 
+let currentYear = 2023; 
+ 
+console.log(getAge(1998)); 
+console.log(getAge(1991)); 
+console.log(getAge(2007));
