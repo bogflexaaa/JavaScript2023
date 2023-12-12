cbe2266319cb2ed3017fd58e26b063936b84ece0
bogflexaaa <127213832+bogflexaaa@users.noreply.github.com>
2023-12-12 23:07:33 +0500

    Add files via upload

--- mod5/task3.js
+++ mod5/task3.js
@@ -1,18 +1,5 @@
-function arrSort(array){ 
-  
-    for (let i = 0; i < array.length; i++){ 
-        for (let j = 0; j < array.length - 1; j++){ 
-            if (array[i+1] < array[j]){ 
-                let temp = array[j]; 
-                array[j] = array[j + 1]; 
-                array[j + 1] = temp; 
-            } 
-        }         
-    } 
- 
-    return array; 
-} 
- 
-console.log(arrSort([1,2,3,4,5])); 
-console.log(arrSort([3,12,33,44,100])); 
-console.log(arrSort([0,1]));
+const arrSort = array => array.sort((a, b) => a - b);
+
+console.log(arrSort([1, 2, 3, 4, 5]));
+console.log(arrSort([3, 12, 33, 44, 100]));
+console.log(arrSort([0, 1]));
